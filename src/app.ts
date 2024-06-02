09d7a214aff30d16a557be2337e58d987509cce8
Luigy Gabriel de Oliveira Ferreira <luigygabriel18a@gmail.com>
2024-06-02 12:26:30 -0300

    feature: Created route and controller for user creation.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,5 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
-const prisma = new PrismaClient()
-
-prisma.user.create({
-  data: {
-    name: '',
-    email: '',
-  },
-})
+app.register(appRoutes)
